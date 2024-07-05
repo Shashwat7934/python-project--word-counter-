444240e1940deb29570b7c6412a6803348db0be0
Shashwat7934 <shashwatkr0804@gmail.com>
2024-07-05 17:53:04 +0530

    Update project4 word counter.py

--- project4 word counter.py
+++ project4 word counter.py
@@ -1,16 +1,16 @@
-string=input("enter the line:")
-def word_count(str):
-    dict={}
-    words=str.split()
+string=input("enter the line:")  #taking input o a paragraph 
+def word_count(str):       #defining a function that counts number of word
+    dict={}           #creating a empty dictionary in which we store the number of occurerence of a particular word
+    words=str.split()   #spliting the words from string
     for word in words:
 
         if word in dict:
-            dict[word] +=1
+            dict[word] +=1  #condition applied if a word is already present then change the value from +1
         else:
-            dict[word]=1
+            dict[word]=1    #else the occurence of word i 1
     return dict
 
-print(word_count(string))
+print(word_count(string))   #print
 
 
 
